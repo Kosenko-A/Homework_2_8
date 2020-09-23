6c871d5a60ca6160b9510c15e71b6909f3251085
Kosenko <Kosenko.2000@yandex.ru>
2020-09-23 22:41:40 +0500

    8 homework))

--- src/ru/geekbrains/homeworks/Main.java
+++ src/ru/geekbrains/homeworks/Main.java
@@ -3,6 +3,19 @@ package ru.geekbrains.homeworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+	doublyLinkedList.add("Harry");
+	doublyLinkedList.add("Ron");
+	doublyLinkedList.add("Hermione");
+	doublyLinkedList.add("Luna");
+
+	System.out.println(doublyLinkedList);
+
+	//doublyLinkedList.remove("Harry");
+	//System.out.println(doublyLinkedList);
+
+	doublyLinkedList.remove("Hermione");
+	System.out.println(doublyLinkedList);
     }
+
 }
